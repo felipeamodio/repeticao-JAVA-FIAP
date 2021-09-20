ae2ff2460172212da5ccf298cc18e6c812d9005d
Felipe Alves <felipeaamodio@gmail.com>
2021-09-20 20:50:50 -0300

    aula repeticao em JAVA

---
+++ src/com/fiap/view/Caixa3.java
@@ -0,0 +1,12 @@
+package com.fiap.view;
+
+public class Caixa3 {
+	public static void main(String[] args) {
+		int qtdProdutos = 5;
+		int registro = 0;
+		
+		for (int i = 0; i < qtdProdutos; i++) {
+			System.out.println("O caixa registrou o produto " + i);
+		}
+	}
+}
